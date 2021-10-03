d271d498c991fe9749aad82673609aa737f078e5
blogSoul <blo9040@gmail.com>
2021-10-04 00:30:23 +0900

    [FEAT] 스프링 핵심 원리 - 기본편 회원 파트 관련 코드 추가
    스프링 핵심 원리 - 기본편 강의에서 실습하는 회원 파트 관련 코드들을 추가함.

---
+++ 스프링 핵심 원리 - 기본편/project/src/main/java/demo/core/MemberApp.java
@@ -0,0 +1,18 @@
+package demo.core;
+
+import demo.core.member.Grade;
+import demo.core.member.Member;
+import demo.core.member.MemberService;
+import demo.core.member.MemberServiceImpl;
+
+public class MemberApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("new Member = " + findMember);
+        System.out.println("find member = " + member);
+    }
+}
---
+++ 스프링 핵심 원리 - 기본편/project/src/main/java/demo/core/member/MemberService.java
@@ -0,0 +1,7 @@
+package demo.core.member;
+
+public interface MemberService {
+    void join(Member member);
+
+    Member findMember(Long memberId);
+}
